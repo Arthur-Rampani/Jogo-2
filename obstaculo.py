201d68705d1c7c988a3a78b30737b4bb66a59b29
Arthur Rampani <arthurrampanicosta@gmail.com>
2024-05-14 11:09:42 -0300

    Add: Fazendo o obstaculo

---
+++ obstaculo.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+
+
+class Obstaculo:
+
+    def __init__(self,arquivo_imagem,largura_imagem,altura_imagem,x_inicial,y_inicial):
+        self.imagem = pygame.image.load(arquivo_imagem)
+
+        self.largura = largura_imagem
+        self.altura = altura_imagem
+
+        self.imagem = pygame.transform.scale(self.imagem,(self.largura,self.altura))
+
+        self.pos_x = x_inicial
+        self.pos_y = y_inicial
+
+        self.velocidade = random.randint(1, 20)
+
+        self.mascara = pygame.mask.from_surface(self.imagem)
+    
+
+    def movimenta(self):
+        self.pos_y = self.pos_y + self.velocidade
+        if self.pos_y < -200:
+            self.pos_y = 850
+            self.velocidade = random.randint(10, 20)
+
+    def desenhar(self, tela):
+        tela.blit(self.imagem,(self.pos_x,self.pos_y))
